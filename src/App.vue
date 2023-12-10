4e1773a06c3d152b4e8e8093d49c133134cce70f
EndCredits <endcredits@crepuscular-aosp.icu>
2023-12-10 12:49:01 +0800

    feat: Add transition animation between routes
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <script setup>
-import NavBar from './components/NavBar.vue';
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
   <NavBar />
   <div>
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <transition name="fade">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
